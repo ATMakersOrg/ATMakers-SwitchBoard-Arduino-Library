42ce269d267f5ca3db0592a539fe2a1a9bdbfe07
ATMakers - Bill Binko <32851154+ATMakersBill@users.noreply.github.com>
2018-03-10 00:57:00 -0500

    Initial Draft
    This is a first stab at the library.  Just the header really - if the inheritance works like I expect, this should be pretty simple with it just inheriting from the MCP23017 and the INA219 (since it has both of those onboard)

---
+++ ATMakers_SwitchBoard.cpp
@@ -0,0 +1,13 @@
+/*************************************************** 
+  This is a library for the ATMakers SwitchBoard Feather Wing
+
+  This Board use I2C to communicate, 2 pins are required to  
+  interface
+  
+  Copyright ATMakers, Inc 2018
+ 
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+
---
+++ ATMakers_SwitchBoard.h
@@ -0,0 +1,38 @@
+/*************************************************** 
+  This is a library for the ATMakers SwitchBoard Feather Wing
+
+  This Board use I2C to communicate, 2 pins are required to  
+  interface
+  
+  Copyright ATMakers, Inc 2018
+ 
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#ifndef _ATMAKERS_SWITCHBOARD_H_
+#define _ATMAKERS_SWITCHBOARD_H_
+
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_INA219.h>
+
+
+int setMap[] = {-1,7,5,3,15,13};
+int resetMap[] = {-1,6,4,2,14,12};
+
+	 
+class ATMakers_SwitchBoard : public Adafruit_MCP23017, public Adafruit_INA219{
+public:
+	
+	inline void relaySet(int relay) { digitalWrite(setMap[relay], HIGH); delay(10); digitalWrite(setMap[relay], LOW);}
+	inline void relayReset(int relay) { digitalWrite(resetMap[relay], HIGH); delay(10);digitalWrite(resetMap[relay], LOW);}
+	
+	boolean currentSensorActive(float thresholdMA=1.0);
+	
+private:
+
+
+
+};
+
+
+#endif
